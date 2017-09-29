9330b73520510f96a093b5b50a2f4f8e48223d7e
Alex Warnes <warnes.alexander@gmail.com>
2017-09-29 12:48:18 -0700

    Almost complete feature for custom tip and tax interface and calculations.

--- src/App.js
+++ src/App.js
@@ -9,25 +9,44 @@ class App extends Component {
       total: 0.0,
       customChecked: false,
       tax: 9,
-      tip: 15
+      tip: 15,
+      subtotal: 0.0
     }
 
     this.calcReceipt = this.calcReceipt.bind(this)
     this.updateCustomChecked = this.updateCustomChecked.bind(this)
+    this.updateCustomTax = this.updateCustomTax.bind(this)
+    this.updateCustomTip = this.updateCustomTip.bind(this)
+    this.updateSubtotal = this.updateSubtotal.bind(this)
+  }
+
+  updateCustomTax (e) {
+    this.setState({tax: e.target.value})
+    this.calcReceipt()
+  }
+
+  updateCustomTip (e) {
+    this.setState({tip: e.target.value})
+    this.calcReceipt()
   }
 
   updateCustomChecked () {
     this.setState({customChecked: !this.state.customChecked})
   }
 
-  calcReceipt (e) {
-    this.setState({total: e.target.value * (1 + 0.09 + 0.15)})
+  updateSubtotal (e) {
+    this.setState({subtotal: e.target.value})
+    this.calcReceipt()
+  }
+
+  calcReceipt () {
+    this.setState({total: this.state.subtotal * (1 + (this.state.tax / 100) + (this.state.tip / 100))})
   }
   render () {
     return (
       <div className="App">
         <h1>Bistromathics!</h1>
-        <input type='text' id='subtotal' placeholder='Subtotal...' onChange={this.calcReceipt}/>
+        <input type='text' id='subtotal' placeholder='Subtotal...' onChange={this.updateSubtotal}/>
         <br/>
         <input type='checkbox' checked={this.state.customChecked} id='customize' onChange={this.updateCustomChecked} />
         <label htmlFor='customize'>Click to set custom values for tip and tax.</label>
